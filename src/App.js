55c69827b74c9caf486d0507bb0d1e4076aa7fa8
Rui Hiroshi Banki <rui@softplan.com.br>
2020-03-13 11:20:41 -0300

    Add the marvel service

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSearchCharacters } from "./hooks/marvelHooks";
 
 function App() {
+  const { data: { results = [] } = {} } = useSearchCharacters();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {results.map(characther => (
+          <li>{characther.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/services/marvelServices.js
@@ -0,0 +1,29 @@
+import CryptoJS from "crypto-js";
+import axios from "axios";
+
+const PRIV_KEY = "c238a0bdde17719c337995a768383c4dfc31bad6";
+const PUBLIC_KEY = "bec0723952e4d4916548c5615d8cbb29";
+const BASE_URL = "http://gateway.marvel.com:80/v1/public";
+
+const getUrl = (path, params) => {
+  const ts = new Date().getTime();
+  const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  const searchParams = new URLSearchParams();
+  searchParams.set("apikey", PUBLIC_KEY);
+  searchParams.set("ts", ts);
+  searchParams.set("hash", hash);
+  for (const key in params) {
+    if (params[key]) {
+      searchParams.set(key, params[key]);
+    }
+  }
+  const url = `${BASE_URL}/${path}?${searchParams.toString()}`;
+  return axios({
+    method: "get",
+    url
+  });
+};
+
+export const searchCharacters = search => {
+  return getUrl("characters", { nameStartsWith: search });
+};
